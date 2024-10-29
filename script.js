60193e873af319e8da49bc35d78ceee421e488b8
AavishRastogi <gamingwiththunderff@gmail.com>
2024-10-29 21:20:50 +0530

    someone is sick

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to display the pop-up
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the pop-up
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
